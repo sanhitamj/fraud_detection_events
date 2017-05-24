5d064fb02b02a6ef6d96e386fa048169ee4780de
Wallace Printz <jnoholds@gmail.com>
2017-05-24 12:56:06 -0500

    updated pipeline bools

--- src/pipeline.py
+++ src/pipeline.py
@@ -52,9 +52,22 @@ class pipeline_json(object):
 
     def _convert_bools(self):
         bin_dict = {0 : False, 1 : True}
-        yn_map = {'y': 1, 'n' : 0}
+        bin_cols = ['show_map', 'fb_published', 'has_logo', 'has_analytics']
+        for col in bin_cols:
+            self.df[col] = self.df[col].map(bin_dict)
 
-        pass
+        yn_dict = {'y': 1, 'n' : 0}
+        yn_cols = ['listed']
+        for col in yn_cols:
+            self.df[col] = self.df[col].map(yn_dict)
+
+        bin_nan_cols = ['has_header']
+        for col in bin_nan_cols:
+            self.df[col] = self.df[col].map(lambda x: x if not np.isnan(x) else -1)
+            self.df[col].astype('int', copy=True)
+
+        payout_dict = {'CHECK': 1, 'ACH' : 1, '':0}
+        self.df['payout_type'] = df['payout_type'].map(payout_dict)
 
     def _add_features(self):
         """
