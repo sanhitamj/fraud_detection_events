7bb3bb846fe5a170555efb798f9751dcb36a6bc6
tylerc-atx <tylerc-atx@github.com>
2017-05-24 10:17:56 -0500

    added docstring to get model

--- get_model.py
+++ get_model.py
@@ -1,3 +1,18 @@
+"""
+Loads a pickled model object from src folder.
+
+Can be used to create a prediction on new data
+
+INPUT:
+    event - (See Features)
+
+OUTPUT:
+    label to STDOUT
+"""
+
+
+import cPickle as pickle
+
 with open('model.pkl') as f:
     model = pickle.load(f)
 
