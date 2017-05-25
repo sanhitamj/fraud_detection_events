13220ff6bf88b30c7a448edee2c0534bcd99c353
Wallace Printz <jnoholds@gmail.com>
2017-05-25 14:22:43 -0500

    updated database

--- src/app.py
+++ src/app.py
@@ -21,16 +21,18 @@ def score():
     json_input = request.get_json()
     tlm = tyler_logit_model()
     prob, prediction = tlm.predict(json_input)
-    print "-" * 50
-    print "RAW"
-    print '{}'.format(json_input)
-    print "-" * 50
-    print "type"
-    print type('{}'.format(json_input))
-    print "-" * 50
-    print "dir"
-    print dir('{}'.format(json_input))
-    insert_vals(prob.astype(np.float64), prediction.astype(int), '{}'.format(json_input), user='tyler')
+    # print "-" * 50
+    # print "RAW"
+    # print '{}'.format(json_input)
+    # print "-" * 50
+    # print "type"
+    # print type('{}'.format(json_input))
+    # print "-" * 50
+    # print "dir"
+    # print dir('{}'.format(json_input))
+    risk_score = tot_payout*prob
+    insert_vals(prob.astype(np.float64), prediction.astype(int), org_name, name, tot_payout.astype(np.float64), risk_score.astype(np.float64), '{}'.format(json_input), user='tyler')
+    # insert_vals(prob.astype(np.float64), prediction.astype(int), '{}'.format(json_input), user='tyler')
 
     return render_template('score.html', name=prediction)
 
